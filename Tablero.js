cd5c4fce4b0f42abd4c3b40b80514c3b71ce8429
Lucas <lucasmunoz.epet20@gmail.com>
2024-04-05 00:38:20 -0300

    fist commit

---
+++ Tablero.js
@@ -0,0 +1,36 @@
+class TableroT{
+
+    constructor(){
+        this.columnas = 10
+        this.filas = 20
+        this.lado = 25
+        this.ancho = this.columnas*this.lado
+        this.alto = this.filas*this.lado
+        this.posicion = createVector(margin_tablero,margin_tablero)
+            }
+
+    coordenada(x,y){
+        return createVector(x,y).mult(this.lado).add(this.posicion)
+            }
+
+    dibujar(){
+        
+        push()
+
+        noStroke()
+        
+        for(let columna = 0; columna < this.columnas; columna++){
+            for (let fila = 0; fila < this.filas; fila++){
+                if((columna+fila)%2==0){
+                    fill("black")
+                }else{
+                    fill("#003")
+                }
+                let z = this.coordenada(columna, fila)
+                    rect (z.x,z.y,this.lado)
+                }
+            
+            }
+        pop()
+    }
+}
