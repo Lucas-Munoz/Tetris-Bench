fa608f7d15a84bcb68584722459764e0564caeef
Lucas-Munoz <lucasmunoz.epet20@gmail.com>
2024-04-13 23:49:56 -0300

    Fix: Tetriminos-Colors

--- Tablero.js
+++ Tablero.js
@@ -22,9 +22,9 @@ class TableroT{
         for(let columna = 0; columna < this.columnas; columna++){
             for (let fila = 0; fila < this.filas; fila++){
                 if((columna+fila)%2==0){
-                    fill("#006")
+                    fill("#96b5ad")
                 }else{
-                    fill("black")
+                    fill("#122f51")
                 }
                 let z = this.coordenada(columna, fila)
                     rect (z.x,z.y,this.lado)
